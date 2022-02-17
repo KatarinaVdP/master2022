ea430dcc81907efb61585ecb12edddb346b3dc25
Oyvind Asplin <oyvind.asplin@gmail.com>
2022-02-17 10:20:31 +0100

    erlend funksjon

---
+++ Old Model/func_erlend.py
@@ -0,0 +1,9 @@
+def print_MSS(object):
+    print("Planning period modified MSS")
+    print("-----------------------------")
+
+    
+
+if __name__=="__main__":
+    print("Hello", end="")
+    print("Hello", end="")
