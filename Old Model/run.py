3d36cda97055d3bab446a8780885a519bc2f1e4d
Katarina Van de Pontseele <katavdp@gmail.com>
2022-02-24 14:02:35 +0100

    Endret mye print out i output_functions

--- Old Model/run.py
+++ Old Model/run.py
@@ -16,26 +16,8 @@ def main(file_name, flexibility, nScenarios, seed, time_limit, new_input=True):
         input_update    = generate_scenarios(input,nScenarios,seed)
         results_update  = categorize_slots(input_update, results_saved)
         print_MSS(input_update, results_update)
-        
-        
-        nFixSlots = sum(results_update["gamm"][s][r][d] for s in input_update["Si"] for r in input_update["Ri"] for d in input_update["Di"])
-        
-        nFixSlotsCategory = sum(results_update["fixedSlot"][r][d] for r in input_update["Ri"] for d in input_update["Di"])
-        nFlexSlotsCategory = sum(results_update["flexSlot"][r][d] for r in input_update["Ri"] for d in input_update["Di"])
-        
-        print("nFixSlots: %i " % nFixSlotsCategory)
-        print("nFlexSlots: %i " % nFlexSlotsCategory)
-        
-        nSlots = (sum(input_update["N"][d] for d in input_update["Di"]) )
-        frac = 1-float(nFixSlots/nSlots)
-        print("Flexible share: %.2f" % frac)
-        print(input_update["F"])
-        print("Printing only gamm slots:")
-        print_GammSlots(input_update, results_update)
-        print("Printing only fixed slots:")
-        print_FixedSlots(input_update, results_update)
-        print("Printing only flexible slots:")
-        print_FlexSlots(input_update, results_update)                    
+        print_expected_operations(input_update, results_update)    
+        #print_expected_bed_util(input_update, results_update)       
                 
     except IOError:
         input = read_input(file_name)
@@ -45,7 +27,8 @@ def main(file_name, flexibility, nScenarios, seed, time_limit, new_input=True):
 
         results, input_update_update = run_model(input_update, flexibility, time_limit)
         results_update  = categorize_slots(input_update_update, results)
-        print(results_update["fixedSlot"])
+        print_MSS(input_update, results_update)
+        print_expected_operations(input_update, results_update)  
 
         with open("Old Model/file.pkl","wb") as f:
             pickle.dump(results,f)
