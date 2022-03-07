1fb72ebf7867501cb8957909d77123a6a453ab1d
Oyvind Asplin <oyvind.asplin@gmail.com>
2022-03-07 18:34:53 +0100

    begynt på skrive til excel funksjon

--- Global_Functions/output_functions.py
+++ Global_Functions/output_functions.py
@@ -1,3 +1,6 @@
+from openpyxl import Workbook
+from openpyxl import load_workbook
+
 def categorize_slots(input_dict, output_dict):
     
     fixed_slots = [[0 for _ in range(input_dict["nDays"])] for _ in range(input_dict["nRooms"])]
@@ -212,3 +215,37 @@ def print_que(input_dict, output_dict):
         print("%s        %s" %(share,min) )
     print()
     print()
+
+##ikke ferdig enda!
+def write_to_excel(file_name,input_dict,output_dict):
+    #filename = "myfile.xlsx"
+    new_row = []
+
+    try:
+        wb = load_workbook(file_name)
+        ws = wb.worksheets[0]  # select first worksheet
+    except FileNotFoundError:
+        headers_row = ['groups','scenarios','seed','flexibility','primal_bound', 'dual_bound','','MC',]
+        for d in input_dict["Di"]:
+            headers_row.append(d)
+        headers_row.append('')
+        headers_row.append('IC')
+        for d in input_dict["Di"]:
+            headers_row.append(d)
+        wb = Workbook()
+        ws = wb.active
+        ws.append(headers_row)
+
+    new_row.append(input_dict["number_of_groups"])
+    new_row.append(input_dict["nScenarios"])
+    new_row.append(input_dict["seed"])
+    new_row.append(input_dict["F"])
+    new_row.append(output_dict["obj"])
+    new_row.append(output_dict["best_bound"])
+    new_row.append('')
+    new_row.append('')
+    """for d in input_dict["Di"]:
+        append"""
+
+    ws.append(new_row)
+    wb.save(file_name)
