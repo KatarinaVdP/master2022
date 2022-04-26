3c670a41fb8ba816c18157732d2095dbf37d2958
Oyvind Asplin <oyvind.asplin@gmail.com>
2022-04-26 16:59:03 +0200

    litt_rydding

--- old_model/model_cutting_stock.py
+++ old_model/model_cutting_stock.py
@@ -4,16 +4,74 @@ from gurobipy import GRB
 from gurobipy import GurobiError
 from gurobipy import quicksum
 from patterns import *
+from functions_output import *
 
-def run_model_cutting_stock(input_dict, flexibility, time_limit):
+def save_results_cutting_stock(m, input_dict, result_dict):
+    #input = copy.deepcopy(input_dict)
+    #result_dict = copy.deepcopy(result_dict2)
+    
+    # ----- Copying the desicion variable values to result dictionary -----
+    result_dict["gamm"] = [[[0 for _ in range(input_dict["nDays"])] for _ in range(input_dict["nRooms"])] for _ in range(input_dict["nSpecialties"])]
+    result_dict["lamb"] = [[[0 for _ in range(input_dict["nDays"])] for _ in range(input_dict["nRooms"])] for _ in range(input_dict["nSpecialties"])]
+    result_dict["delt"] = [[[[0 for _ in range(input_dict["nScenarios"])] for _ in range(input_dict["nDays"])] for _ in range(input_dict["nRooms"])] for _ in range(input_dict["nSpecialties"])]
+    result_dict["x"]    = [[[[0 for _ in range(input_dict["nScenarios"])] for _ in range(input_dict["nDays"])] for _ in range(input_dict["nRooms"])] for _ in range(input_dict["nGroups"])]
+    result_dict["a"]    = [[0 for _ in range(input_dict["nScenarios"])] for _ in range(input_dict["nGroups"])]
+    result_dict["v"]    = [[0 for _ in range(input_dict["nDays"])] for _ in range(input_dict["nWards"])]
+    
+    
+    for s in input_dict["Si"]:
+        for r in input_dict["Ri"]:
+            for d in input_dict["Di"]:
+                name = (f"gamma[{s},{r},{d}]")
+                var= m.getVarByName(name)    
+                result_dict["gamm"][s][r][d] = var.X
+                name = (f"lambda[{s},{r},{d}]")
+                var= m.getVarByName(name) 
+                result_dict["lamb"][s][r][d] = var.X
+                
+                for c in input_dict["Ci"]:
+                    name = (f"delta[{s},{r},{d},{c}]")
+                    var= m.getVarByName(name)  
+                    result_dict["delt"][s][r][d][c] = var.X        
+    
+    for g in input_dict["Gi"]:
+        for r in input_dict["Ri"]:
+            for d in input_dict["Di"]:    
+                for c in input_dict["Ci"]:
+                    val = 0
+                    for mi in input_dict["Mi"]:
+                        name = (f"pat[{mi},{r},{d},{c}]")
+                        var= m.getVarByName(name)
+                        val += input_dict["A"][mi][g]*var.X
+                    if val > 0:
+                        result_dict["x"][g][r][d][c] = int(val)
+                    
+    for g in input_dict["Gi"]:
+        for c in input_dict["Ci"]:
+            name = (f"a[{g},{c}]")
+            var= m.getVarByName(name)
+            result_dict["a"][g][c] = var.X
+                
+    for w in input_dict["Wi"]:
+        for d in range(input_dict["J"][w]-1):
+            result_dict["v"][w][d] = sum(input_dict["Pi"][c] * sum(input_dict["P"][w][g][d+input_dict["nDays"]-dd] * result_dict["x"][g][r][dd][c] for g in input_dict["GWi"][w] for r in input_dict["Ri"] for dd in range(d+input_dict["nDays"]+1-input_dict["J"][w],input_dict["nDays"])) for c in input_dict["Ci"]) 
+    
+    result_dict["bed_occupation_wdc"]   =   [[[0 for _ in range(input_dict["nScenarios"])] for _ in range(input_dict["nDays"])] for _ in range(input_dict["nWards"])]
+    result_dict["bed_occupation"]       =   [[0 for _ in range(input_dict["nDays"])] for _ in range(input_dict["nWards"])]
+    for w in input_dict["Wi"]:
+        for d in input_dict["Di"]:
+            for c in input_dict["Ci"]:
+                result_dict["bed_occupation_wdc"][w][d][c]  =   sum(input_dict["P"][w][g][d-dd] * result_dict["x"][g][r][dd][c] for g in input_dict["GWi"][w] for r in input_dict["Ri"] for dd in range(max(0,d+1-input_dict["J"][w]),d+1)) + input_dict["Y"][w][d]
+    for w in input_dict["Wi"]:
+            for d in input_dict["Di"]:
+                result_dict["bed_occupation"][w][d]         =   sum(result_dict["bed_occupation_wdc"][w][d][c]*input_dict["Pi"][c] for c in input_dict["Ci"])
+    
+    return result_dict
+
+def run_model_cutting_stock(input_dict, flexibility, time_limit,print_optimizer=False):
     input=input_dict
     #----- Sets ----- #  
     nDays           =   input["nDays"]
-    nWards          =   input["nWards"]
-    nScenarios      =   input["nScenarios"]
-    nGroups         =   input["nGroups"]
-    nSpecialties    =   input["nSpecialties"]
-    nRooms          =   input["nRooms"]
     
     Wi      =   input["Wi"]
     Si      =   input["Si"]
@@ -29,16 +87,11 @@ def run_model_cutting_stock(input_dict, flexibility, time_limit):
     #----- Parameter ----- #  
     F   =   flexibility
     input["F"]  = flexibility
-    E   =   input["E"]
-    TC  =   input["TC"]
     I   =   input["I"]
     B   =   input["B"]
-    H   =   input["H"]
     K   =   input["K"]
-    L   =   input["L"]
     U   =   input["U"]
     N   =   input["N"]
-    T   =   input["T"]
     Co  =   input["Co"]
     J   =   input["J"]
     Pi  =   input["Pi"]
@@ -46,7 +99,7 @@ def run_model_cutting_stock(input_dict, flexibility, time_limit):
     P   =   input["P"]
     Y   =   input["Y"]
     nFixed = int(np.ceil((1-F) * sum(N[d] for d in Di)/I)*I)
-    input_dict["nFixed"] = nFixed
+    input["nFixed"] = nFixed
     
     # Parameters and sets specific to cutting stock model
     A       =   input["A"]
@@ -60,6 +113,9 @@ def run_model_cutting_stock(input_dict, flexibility, time_limit):
     #----- Model ----- #
     m = gp.Model("mss_mip")
     m.setParam("TimeLimit", time_limit)
+    
+    if not print_optimizer:
+        m.Params.LogToConsole = 0
     
     '--- Variables ---'
     gamm    =   m.addVars(Si, Ri, Di, vtype=GRB.BINARY, name="gamma")
@@ -86,7 +142,7 @@ def run_model_cutting_stock(input_dict, flexibility, time_limit):
     m.ModelSense = GRB.MINIMIZE 
     '--- Constraints ---'
     m.addConstr(
-        quicksum( quicksum(gamm[s,r,d] for r in RSi[s]) for s in Si for d in Di) - (1-F) * quicksum(N[d] for d in Di)  >= 0,
+        quicksum( quicksum(gamm[s,r,d] for r in RSi[s]) for s in Si for d in Di) ==  nFixed ,
         name = "Con_PercentFixedRooms"
         )
     for s in Si:
@@ -100,7 +156,7 @@ def run_model_cutting_stock(input_dict, flexibility, time_limit):
         for s in Si),
         name = "Con_LongDaysCap",
     )
-    print('still Creating Model 30%')
+    print('Creating model (1/3)')
     m.addConstrs(
         (quicksum(gamm[s,r,d]+delt[s,r,d,c] for s in Si)<= 1 
         for r in Ri for d in Di for c in Ci),
@@ -136,7 +192,7 @@ def run_model_cutting_stock(input_dict, flexibility, time_limit):
         for r in Ri for d in Di for c in Ci),
         name= "Con_OnlyAssignIfNecessary",
     )
-    print('still Creating Model 60%')
+    print('Creating model (2/3)')
     m.addConstrs(
         (quicksum(Psum[m][w][d-dd] * pat[m,r,dd,c] for m in Mi for r in Ri for dd in range(max(0,d+1-J[w]),d+1)) <= B[w][d] - Y[w][d] 
         for w in Wi for d in Di for c in Ci),
@@ -154,11 +210,18 @@ def run_model_cutting_stock(input_dict, flexibility, time_limit):
             for r in RSi[s] for d in range(0,int(nDays-nDays/I))),
         name = "Con_RollingExtendedSlotCycles" + str(s),
         )
-    print('Model Created 100%')
-
+    print('Creating model (3/3)')
     m.optimize()
+    result_dict = save_results_pre(m)
 
-    statuses=[0,"LOADED","OPTIMAL","INFEASIBLE","INF_OR_UNBD","UNBOUNDED","CUTOFF", "ITERATION_LIMIT",
+    nSolutions=m.SolCount
+    if nSolutions==0:
+        result_dict["status"]=0
+    else:
+        result_dict =  save_results_cutting_stock(m, input_dict, result_dict)
+        
+    return result_dict, input    
+    """statuses=[0,"LOADED","OPTIMAL","INFEASIBLE","INF_OR_UNBD","UNBOUNDED","CUTOFF", "ITERATION_LIMIT",
     "NODE_LIMIT", "TIME_LIMIT", "SOLUTION_LIMIT","INTERUPTED","NUMERIC","SUBOPTIMAL", "USES_OBJ_LIMIT","WORK_LIMIT"]
     result_dict =   {}
     result_dict["status"]=statuses[m.STATUS]
@@ -170,51 +233,50 @@ def run_model_cutting_stock(input_dict, flexibility, time_limit):
     result_dict["runtime"] = m.Runtime
     result_dict["max_runtime"] = time_limit   
     result_dict["MIPGap"] = m.MIPGap  
-    nSolutions=m.SolCount
+    nSolutions=m.SolCount"""
     
-    if nSolutions==0:
+    """if nSolutions==0:
         result_dict["status"]=statuses[0]
         print('0 solutions found')
-    else:
-        # ----- Copying the desicion variable values to result dictionary -----
-        result_dict["gamm"] = [[[0 for _ in range(input["nDays"])] for _ in range(input["nRooms"])] for _ in range(input["nSpecialties"])]
-        result_dict["lamb"] = [[[0 for _ in range(input["nDays"])] for _ in range(input["nRooms"])] for _ in range(input["nSpecialties"])]
-        result_dict["delt"] = [[[[0 for _ in range(input["nScenarios"])] for _ in range(input["nDays"])] for _ in range(input["nRooms"])] for _ in range(input["nSpecialties"])]
-        result_dict["x"]    = [[[[0 for _ in range(input["nScenarios"])] for _ in range(input["nDays"])] for _ in range(input["nRooms"])] for _ in range(input["nGroups"])]
-        result_dict["a"]    = [[0 for _ in range(input["nScenarios"])] for _ in range(input["nGroups"])]
-        result_dict["v"]    = [[0 for _ in range(input["nDays"])] for _ in range(input["nWards"])]
-        
-        for s in input["Si"]:
-            for r in input["Ri"]:
-                for d in input["Di"]:    
-                    result_dict["gamm"][s][r][d] = gamm[s,r,d].getAttr("x")
-                    result_dict["lamb"][s][r][d] = lamb[s,r,d].X
-                    for c in input["Ci"]:
-                        result_dict["delt"][s][r][d][c] = delt[s,r,d,c].X                     
-        for g in input["Gi"]:
-            for r in input["Ri"]:
-                for d in input["Di"]:    
-                    for c in input["Ci"]:
-                        val = sum(A[m][g]*pat[m,r,d,c].X for m in Mi)
-                        if val > 0:
-                            result_dict["x"][g][r][d][c] = val
-        for g in input["Gi"]:
-            for c in input["Ci"]:
-                result_dict["a"][g][c] = a[g,c].X
-                    
-        for w in Wi:
-            for d in range(J[w]-1):
-                result_dict["v"][w][d] = sum(Pi[c] * sum(P[w][g][d+nDays-dd] * result_dict["x"][g][r][dd][c] for g in GWi[w] for r in Ri for dd in range(d+nDays+1-J[w],nDays)) for c in Ci) 
-        
-        bed_occupationC =[[[0 for _ in range(input_dict["nScenarios"])] for _ in range(input_dict["nDays"])] for _ in range(input_dict["nWards"])]
-        result_dict["bed_occupation"] =[[0 for _ in range(input_dict["nDays"])] for _ in range(input_dict["nWards"])]
-        for w in input_dict["Wi"]:
+    else:"""
+    """# ----- Copying the desicion variable values to result dictionary -----
+    result_dict["gamm"] = [[[0 for _ in range(input["nDays"])] for _ in range(input["nRooms"])] for _ in range(input["nSpecialties"])]
+    result_dict["lamb"] = [[[0 for _ in range(input["nDays"])] for _ in range(input["nRooms"])] for _ in range(input["nSpecialties"])]
+    result_dict["delt"] = [[[[0 for _ in range(input["nScenarios"])] for _ in range(input["nDays"])] for _ in range(input["nRooms"])] for _ in range(input["nSpecialties"])]
+    result_dict["x"]    = [[[[0 for _ in range(input["nScenarios"])] for _ in range(input["nDays"])] for _ in range(input["nRooms"])] for _ in range(input["nGroups"])]
+    result_dict["a"]    = [[0 for _ in range(input["nScenarios"])] for _ in range(input["nGroups"])]
+    result_dict["v"]    = [[0 for _ in range(input["nDays"])] for _ in range(input["nWards"])]
+    
+    for s in input["Si"]:
+        for r in input["Ri"]:
+            for d in input["Di"]:    
+                result_dict["gamm"][s][r][d] = gamm[s,r,d].getAttr("x")
+                result_dict["lamb"][s][r][d] = lamb[s,r,d].X
+                for c in input["Ci"]:
+                    result_dict["delt"][s][r][d][c] = delt[s,r,d,c].X                     
+    for g in input["Gi"]:
+        for r in input["Ri"]:
+            for d in input["Di"]:    
+                for c in input["Ci"]:
+                    val = sum(A[m][g]*pat[m,r,d,c].X for m in Mi)
+                    if val > 0:
+                        result_dict["x"][g][r][d][c] = val
+    for g in input["Gi"]:
+        for c in input["Ci"]:
+            result_dict["a"][g][c] = a[g,c].X
+                
+    for w in Wi:
+        for d in range(J[w]-1):
+            result_dict["v"][w][d] = sum(Pi[c] * sum(P[w][g][d+nDays-dd] * result_dict["x"][g][r][dd][c] for g in GWi[w] for r in Ri for dd in range(d+nDays+1-J[w],nDays)) for c in Ci) 
+    
+    bed_occupationC =[[[0 for _ in range(input_dict["nScenarios"])] for _ in range(input_dict["nDays"])] for _ in range(input_dict["nWards"])]
+    result_dict["bed_occupation"] =[[0 for _ in range(input_dict["nDays"])] for _ in range(input_dict["nWards"])]
+    for w in input_dict["Wi"]:
+        for d in input_dict["Di"]:
+            for c in input_dict["Ci"]:
+                bed_occupationC[w][d][c]= sum(input_dict["P"][w][g][d-dd] * result_dict["x"][g][r][dd][c] for g in input_dict["GWi"][w] for r in input_dict["Ri"] for dd in range(max(0,d+1-input_dict["J"][w]),d+1)) + input_dict["Y"][w][d]
+    result_dict["bed_occupation_wdc"] = bed_occupationC
+    for w in input_dict["Wi"]:
             for d in input_dict["Di"]:
-                for c in input_dict["Ci"]:
-                    bed_occupationC[w][d][c]= sum(input_dict["P"][w][g][d-dd] * result_dict["x"][g][r][dd][c] for g in input_dict["GWi"][w] for r in input_dict["Ri"] for dd in range(max(0,d+1-input_dict["J"][w]),d+1)) + input_dict["Y"][w][d]
-        result_dict["bed_occupation_wdc"] = bed_occupationC
-        for w in input_dict["Wi"]:
-                for d in input_dict["Di"]:
-                    result_dict["bed_occupation"][w][d] = sum(bed_occupationC[w][d][c]*input_dict["Pi"][c] for c in input_dict["Ci"])
-
-    return result_dict, input           
+                result_dict["bed_occupation"][w][d] = sum(bed_occupationC[w][d][c]*input_dict["Pi"][c] for c in input_dict["Ci"])"""
+       
