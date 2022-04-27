bb19d4f26a14bb4e3ba921a847fa535215748b23
Oyvind Asplin <oyvind.asplin@gmail.com>
2022-04-27 11:00:30 +0200

    problem size changes

--- old_model/problem_size.py
+++ old_model/problem_size.py
@@ -38,27 +38,27 @@ def write_to_excel_problem_size(excel_file_name: str, results_mip: dict, flex: i
     ws.append(new_row)
     wb.save(excel_file_name)  
 
-num_groups                  =   [9, 25]
-num_scenarios               =   [10,50,100,200]
+num_groups                  =   [9]
+num_scenarios               =   [2,3,4,5]
 bed_caps                    =   [1, 0.5]
 flexibilities               =   [0.1]
 
-time_to_mip                 =   20
+time_to_mip                 =   10
 seed                        =   1
 excel_file_name             =   'input_output/problem_size.xlsx'
 
-for ng in num_groups:
-    input_file_name         =   choose_correct_input_file(ng)
-    input                   =   read_input(input_file_name)
-    if ng ==   25:
-        change_demand(input, 1.35, print_minutes = False)
-    for ns in num_scenarios:
-        input               =   generate_scenarios(input, ns, seed)
-        for flex in flexibilities:
-            for cap in bed_caps:
-                input                   =   change_ward_capacity(input, "MC", 60*cap,  49*cap)
-                input                   =   change_ward_capacity(input, "IC", 11*cap,  7*cap)
+for cap in bed_caps:
+    for ng in num_groups:
+        input_file_name         =   choose_correct_input_file(ng)
+        input                   =   read_input(input_file_name)
+        input                   =   change_ward_capacity(input, "MC", 60*cap,  49*cap)
+        input                   =   change_ward_capacity(input, "IC", 11*cap,  7*cap)
+        if ng ==   25:
+            change_demand(input, 1.35, print_minutes = False)
+        for ns in num_scenarios:
+            input               =   generate_scenarios(input, ns, seed)
+            for flex in flexibilities:
                 results, input          =   run_model_mip(input,flex,time_to_mip,expected_value_solution=False,print_optimizer = False)
                 write_to_excel_problem_size(excel_file_name, results,flex,ns,seed,cap)
                 print("nGroups: %i  nScenarios: %i  flex: %.2f  bed_cap_factor: %.2f  primal: %.1f  dual: %.1f MIPgap: %.3f runtime: %.1f "%(ng,ns,flex,cap, results["obj"], results["best_bound"], results["MIPGap"],results["runtime"]))
-        
+            
