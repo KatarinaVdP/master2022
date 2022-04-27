5d0c5a91d0f659410c84ff1197c40e9fbba8c64d
ErlendJoCo <erlend.corneliussen@outlook.com>
2022-04-27 16:07:10 +0200

    Test_run oppdatert

--- old_model/test_run.py
+++ old_model/test_run.py
@@ -1,15 +1,24 @@
+from random import seed
 from functions_input import *
 from functions_output import *
 from model_cutting_stock import *
 from model_mip import *
 
 
-input_file_name =   choose_correct_input_file(9)
+nGroups     =   9
+nScenarios  = 100
+seed        = 1
+flex        = 0.10
+time_limit  = 1200
+
+
+input_file_name =   choose_correct_input_file(nGroups)
 input           =   read_input(input_file_name)
-input           =   generate_scenarios(input, 10, 1)
-input           =   edit_input_to_number_of_groups(input, 9)
-#results, input  =   run_model_mip(input,0.1,60,expected_value_solution=False, print_optimizer = True)
-results, input  =   run_model_cutting_stock(input,0.1,40,print_optimizer = True)
+input           =   generate_scenarios(input, nScenarios, seed)
+input           =   edit_input_to_number_of_groups(input, nGroups)
+results, input  =   run_model_mip(input, flex, time_limit, expected_value_solution = False, print_optimizer = True)
+#results, input  =   run_model_cutting_stock(input,0.1,40,print_optimizer = True)
 results         =   categorize_slots(input, results)
+print_MSS(input, results)
 
 print(results["x"])
