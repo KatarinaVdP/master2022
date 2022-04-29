de7952fbe843e25e0a6dcb3564d0b8d5e2489cfe
Oyvind Asplin <oyvind.asplin@gmail.com>
2022-04-29 17:18:25 +0200

    naa_klar_til_a_kjore

--- old_model/test_run_parameter_tuning.py
+++ old_model/test_run_parameter_tuning.py
@@ -1,6 +1,8 @@
 from functions_input import *
 from functions_output import *
 from model_cutting_stock import *
+from run_second_stage_pattern import *
+from run_second_stage_mip import *
 from model_mip import *
 import random
 
@@ -10,7 +12,6 @@ def test_run(temp0:float,alph:float,i:int,temp1:float):
     obj_best        =   str(temp0)+ " " + str(alph)+ " " + str(i)+ " " + str(temp1) + " obj best" + str(random. randint(0,100))
     time_obj_best   =   random. randint(0,100)
     return obj,time_obj,obj_best,time_obj_best
-    
     
 def write_to_excel_parameter_tuning(excel_file_name: str, num_runs: int, initial_temp: int, alpha: float, i_max: int, min_temp: float, objectives: list, time_objectives: list, best_objectives: list, time_best_objectives: list):
     try:
@@ -73,16 +74,18 @@ def write_to_excel_parameter_tuning(excel_file_name: str, num_runs: int, initial
     
 
 num_groups                  =   9
-num_scenarios               =   250
+num_scenarios               =   2
 flex                        =   0.1
+seed                        =   1
+time_limit_EVS_and_fixed    =   10
 
-excel_file_name             =   'input_output/parameter_tuning_heuristic_mip.xlsx'
+excel_file_name             =   'input_output/parameter_tuning_heuristic_pattern.xlsx'
 
 num_runs                    =   30
 initial_temp                =   [5,10,100]
 alpha                       =   [0.5, 0.75]
 i_max                       =   [25,50]
-end_temp                    =   [0.1, 0.01]
+end_temp                    =   [0.1,0.01,0.001]
 
 for temp0 in initial_temp:
     for alph in alpha:
@@ -93,10 +96,12 @@ for temp0 in initial_temp:
                 best_objectives         =   []
                 time_best_objectives    =   []
                 for run in range(num_runs):
-                    obj, time_obj, best_obj, time_best_obj = test_run(temp0,alph,i,temp1)
-                    objectives.append(obj)
-                    time_objectives.append(time_obj)
-                    best_objectives.append(best_obj)
-                    time_best_objectives.append(time_best_obj)
+                    #obj, time_obj, best_obj, time_best_obj = test_run(temp0,alph,i,temp1)
+                    end_results, global_best_results = run_second_stage_pattern(flex, num_groups, num_scenarios, seed,time_limit_EVS_and_fixed, temp0, alph, i, temp1)
+                    #end_results, global_best_results = run_second_stage_mip(flex, num_groups, num_scenarios, seed, time_limit_EVS_and_fixed, temp0, alph, i, temp1)
+                    objectives.append(end_results["obj"])
+                    time_objectives.append(end_results["runtime"])
+                    best_objectives.append(global_best_results["obj"])
+                    time_best_objectives.append(global_best_results["runtime"])
                 write_to_excel_parameter_tuning(excel_file_name,        num_runs, temp0, alph, i, temp1,       objectives, time_objectives, best_objectives, time_best_objectives)
                     
