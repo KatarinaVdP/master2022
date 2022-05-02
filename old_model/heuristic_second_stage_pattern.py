379c2901fb34d4785338ea27b76074463b0cd270
Oyvind Asplin <oyvind.asplin@gmail.com>
2022-05-02 15:09:47 +0200

    ny_greie

--- old_model/heuristic_second_stage_pattern.py
+++ old_model/heuristic_second_stage_pattern.py
@@ -108,6 +108,96 @@ def heuristic_second_stage_pattern(excel_file, input_dict, results, start_temper
     best_sol["runtime"] = time.time() - start_time  
     return best_sol, global_best_sol
 
+def heuristic_second_stage_pattern_param_tuning(input_dict, results, start_temperature, alpha, iter_max, end_temperature):
+    input=input_dict
+    start_time = time.time()
+    best_sol = copy.deepcopy(results)
+    global_iter = 1
+    swap_types = ["fix", "ext", "flex"]
+    levels = math.ceil(np.log(end_temperature)/np.log(alpha))
+    
+    #----- Looping through and making all possible swap_fixed_with_flexible_UR_KA_EN swaps -----
+    print("\n\nThe following changes are made due to swap_fixed_with_flexible_UR_KA_EN:\n\n")
+    swap_ever_found = False
+    days_in_cycle = int(input["nDays"]/input["I"])
+    for d in range(days_in_cycle):
+        swap_found, getting_slot, giving_slot, extended = swap_fixed_with_flexible_UR_KA_EN(d, input_dict, best_sol, print_swap = False)
+        if swap_found:
+            swap_ever_found = True
+            swap_type = "flex"
+            results = change_bound_second_stage_pattern(results, swap_found, getting_slot, giving_slot, swap_type, extended)
+            results = run_greedy_construction_heuristic(input, results)
+            results = translate_heristic_results(input,results)
+            results = categorize_slots(input, results)
+            
+    if swap_ever_found:
+        action = "MOVE"
+    else:
+        action = "NO MOVE" 
+    best_sol = copy.deepcopy(results)
+    
+    #----- Looping through temperature levels ----- 
+    level = 1
+    temperature = copy.deepcopy(start_temperature)
+    global_best_sol = copy.deepcopy(best_sol)
+    
+    while temperature >= end_temperature * start_temperature:
+        iter = 1
+        #----- Looping through iterations at temperature level -----
+        while iter <= iter_max:
+            
+            extended = False
+            swap_type = np.random.choice(swap_types)
+            if swap_type == "ext":
+                swap_found, getting_slot, giving_slot = swap_extension(input_dict, best_sol, print_swap = False)
+            elif swap_type == "fix":
+                swap_found, getting_slot, giving_slot = swap_fixed(input_dict, best_sol, print_swap = False)
+            elif swap_type == "flex":
+                swap_found, getting_slot, giving_slot, extended = swap_fixed_with_flexible(input_dict, best_sol, print_swap = False)
+            
+            #----- Changing variable bound to evaluate candidate -----
+            results = change_bound_second_stage_pattern(results, swap_found, getting_slot, giving_slot, swap_type, extended)
+            results = run_greedy_construction_heuristic(input, results)
+            
+            #----- Storing entire solution if a new best solution is found -----
+            pick_worse_obj = rand.random()
+            delta = results["obj"] - best_sol["obj"]
+            try:
+                exponential = math.exp(-delta/temperature)
+            except:
+                exponential = 0
+            if delta == 0:
+                exponential = 0
+                
+            if results["obj"]+0.1 < best_sol["obj"] or pick_worse_obj < exponential:
+                
+                if results["obj"] < global_best_sol["obj"]: 
+                    global_best_sol = copy.deepcopy(results)
+                    global_best_sol["runtime"] = time.time() - start_time  
+                
+                if results["obj"]+0.1 < best_sol["obj"]:
+                    action = "MOVE"
+                else:
+                    action = "MOVE*"
+            
+                best_sol = copy.deepcopy(results)
+            else:
+                # ----- Copying the desicion variable values to result dictionary -----
+                action = "NO MOVE"
+                results = change_bound_second_stage_pattern(results, swap_found, getting_slot, giving_slot, swap_type, extended, swap_back = True)
+            
+            # ----- Printing iteration to console -----
+            current_time = (time.time()-start_time)
+            print_heuristic_iteration(best_sol["obj"], results["obj"], swap_type, action, current_time, global_iter, level, levels, iter, iter_max)
+            iter += 1
+            global_iter += 1 
+            
+        level += 1    
+        temperature = update_temperature(temperature, alpha)
+        
+    best_sol["runtime"] = time.time() - start_time  
+    return best_sol, global_best_sol
+
 def change_bound_second_stage_pattern(results, swap_found, getting_slot, giving_slot, swap_type, extended, swap_back = False):
     
     if swap_type == "ext":
@@ -191,7 +281,6 @@ def change_bound_second_stage_pattern(results, swap_found, getting_slot, giving_
 def run_second_stage_pattern_param_tuning(flexibility: float, number_of_groups: int, nScenarios: int, seed: int, time_limit: int, temperature, alpha, iter_max, end_temperature, new_input=True,parameter_tuning=False):
     print("\n\n")
     input_file_name =   choose_correct_input_file(number_of_groups)
-    excel_file      =   "input_output/" + "results.xlsx"
     input           =   read_input(input_file_name)
 
     #----- Try to load initial model run from earlier ----  
@@ -210,7 +299,7 @@ def run_second_stage_pattern_param_tuning(flexibility: float, number_of_groups: 
             input = saved_values["input"]
             results = saved_values["results"]
             print("INITIATING GREEDY HEURISTIC SEARCH FROM EVS - USING PICKLED RESULTS")
-            results, global_best_sol = heuristic_second_stage_pattern(excel_file, input, results, temperature, alpha, iter_max, end_temperature) # --- swap is called inside 
+            results, global_best_sol = heuristic_second_stage_pattern_param_tuning(input, results, temperature, alpha, iter_max, end_temperature) # --- swap is called inside 
 
     #----- If initial model run is not found, run as usual -----   
     else:
@@ -231,7 +320,8 @@ def run_second_stage_pattern_param_tuning(flexibility: float, number_of_groups: 
         
         #----- Begin Heuristic ----  
         print("INITIATING GREEDY HEURISTIC SEARCH FROM EVS")
-        results, global_best_sol = heuristic_second_stage_pattern(excel_file, input, results, temperature, alpha, iter_max, end_temperature)
+        
+        results, global_best_sol = heuristic_second_stage_pattern_param_tuning(input, results, temperature, alpha, iter_max, end_temperature)
     return results, global_best_sol
 
 def run_second_stage_pattern(flexibility: float, number_of_groups: int, nScenarios: int, seed: int, time_limit: int, temperature, alpha, iter_max, end_temperature, new_input=True,parameter_tuning=False):
